d666f32065f1fef390ef86345c07a1e4aa3dd536
tomas <tomas.gaalvez@gmail.com>
2025-09-21 17:35:45 -0300

    Initial config: Install tailwind, create main page, header, and folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './features/main-page/main-page';
+import { UserProfile } from './features/user/user-profile/user-profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: MainPage },
+  { path: 'perfil-usuario', component: UserProfile },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+];
