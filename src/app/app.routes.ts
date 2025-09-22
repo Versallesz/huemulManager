0a8c45919279c0b570b89475dcd44e3efc68689e
tomas <tomas.gaalvez@gmail.com>
2025-09-22 14:41:34 -0300

    crear componentes de login y signup, crear formulario de login y layouts para auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './features/main-page/main-page';
 import { UserProfile } from './features/user/user-profile/user-profile';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout';
+import { Login } from './core/login/login';
+import { SignUp } from './core/sign-up/sign-up';
 
 export const routes: Routes = [
-  { path: 'inicio', component: MainPage },
-  { path: 'perfil-usuario', component: UserProfile },
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'inicio', component: MainPage },
+      { path: 'perfil-usuario', component: UserProfile },
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'sign-up', component: SignUp }
+    ]
+  }
 ];
---
+++ src/app/core/login/login.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Here goes the server logic
+    }
+  }
+}
